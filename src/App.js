b8c5e0d008d8b43404383d3db554c252845843d1
zxwgm <jeff0630@naver.com>
2023-12-21 13:01:53 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "카카오톡";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="react">{name}</div>
+      <div className="hi">카톡 안녕가이드 ㅣ 카톡설명서 ㅣ 다운로드</div>
+      <div className="all"></div>
+
+      <div className="center">
+        <div className="shop">카카오쇼핑</div>
+        <div className="letter">
+          카톡에서 이뤄지는 오늘의 쇼핑 4번째 랩 '카카오쇼핑'에서 제공하는
+          제품을 빠르고 간편하게 살펴보세요
+        </div>
+      </div>
+    </>
   );
 }
 
